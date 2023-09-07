2c71cc97a17138dae0dd9edf1bdc10829ac95525
ZeusOnline1998 <zeusonline1998@gmail.com>
2023-09-07 20:20:42 +0530

    Auth Token Check on every run of the main page so user can have relevant access

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,14 +1,34 @@
 import React, { useEffect } from 'react'
 import { useNavigate, Outlet } from 'react-router-dom';
 import NavBar from './NavBar'
+import axios from 'axios';
 
 const Dashboard = () => {
 
   const navigate = useNavigate();
+  const host = process.env.REACT_APP_SERVER_URL
 
   useEffect(() => {
     if (localStorage.getItem('token')) {
       //Do nothing;
+      axios.get(`${host}/api/check_access_token/`, {
+        headers: {
+          'Authorization': `JWT ${localStorage.getItem('token')}`
+        }
+      })
+        .then((response) => {
+          // console.log(response.data)
+          return response.data
+        }).then(data => {
+          if (data.access && data.refresh){
+            localStorage.setItem('token', data.access)
+            localStorage.setItem('refresh', data.refresh)
+          }
+          else{
+            navigate('/login');
+          }
+        }
+        )
     }
     else {
       navigate("/login");
